ec711642b6a5aa8e10a046ded955be0b20778382
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-03-20 21:16:17 +0100

    Update CountryServiceImpl.java

--- src/main/java/mk/finki/ukim/libraryemtlab/service/CountryServiceImpl.java
+++ src/main/java/mk/finki/ukim/libraryemtlab/service/CountryServiceImpl.java
@@ -2,34 +2,50 @@ package mk.finki.ukim.libraryemtlab.service;
 
 import mk.finki.ukim.libraryemtlab.model.Country;
 import mk.finki.ukim.libraryemtlab.model.dto.CountryDto;
+import mk.finki.ukim.libraryemtlab.repository.CountryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CountryServiceImpl implements CountryService{
+   private final CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+
     @Override
     public List<Country> findAllCountries() {
-        return null;
+        return countryRepository.findAll();
     }
 
     @Override
     public Country findCountryById(Long country) {
-        return null;
+        return countryRepository.findById(country).orElseThrow(()->new RuntimeException());
     }
 
     @Override
     public Country addCountry(CountryDto country) {
-        return null;
+        Country country1=new Country();
+        country1.setContinent(country.getContinent());
+        country1.setName(country.getName());
+
+        return this.countryRepository.save(country1);
     }
 
     @Override
     public Country editCountry(Long id, CountryDto country) {
-        return null;
+
+        Country country1=countryRepository.findById(id).orElseThrow(()-> new RuntimeException());
+        country1.setContinent(country.getContinent());
+        country1.setName(country.getName());
+        return this.countryRepository.save(country1);
     }
 
     @Override
     public void deleteCountry(Long id) {
-
+        this.countryRepository.deleteById(id);
     }
 }
