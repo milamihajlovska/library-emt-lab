b610f251d341adaef5a9f4855b74a3df3d19540b
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-03-20 21:21:45 +0100

    Update BookServiceImpl.java

--- src/main/java/mk/finki/ukim/libraryemtlab/service/BookServiceImpl.java
+++ src/main/java/mk/finki/ukim/libraryemtlab/service/BookServiceImpl.java
@@ -2,6 +2,8 @@ package mk.finki.ukim.libraryemtlab.service;
 
 import mk.finki.ukim.libraryemtlab.model.Book;
 import mk.finki.ukim.libraryemtlab.model.dto.BookDto;
+import mk.finki.ukim.libraryemtlab.repository.AuthorRepository;
+import mk.finki.ukim.libraryemtlab.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,33 +11,55 @@ import java.util.Optional;
 
 @Service
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    public BookServiceImpl(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
     @Override
     public List<Book> findAllBooks() {
-        return null;
+        return this.bookRepository.findAll();
     }
 
     @Override
     public Optional<Book> findBookById(Long id) {
-        return Optional.empty();
+        return this.bookRepository.findById(id);
     }
 
     @Override
     public Book addBook(BookDto bookDto) {
-        return null;
+
+        Book book=new Book();
+        book.setAuthor(this.authorRepository.findById(bookDto.getAuthor()).orElseThrow(()->new RuntimeException()));
+        book.setName(bookDto.getName());
+        book.setCategory(bookDto.getCategory());
+        book.setAvailableCopies(bookDto.getAvailableCopies());
+        return this.bookRepository.save(book);
+
     }
 
     @Override
     public Book editBook(Long id, BookDto bookDto) {
-        return null;
+        Book book=this.bookRepository.findById(id).orElseThrow(()->new RuntimeException());
+        book.setAuthor(this.authorRepository.findById(bookDto.getAuthor()).orElseThrow(()->new RuntimeException()));
+        book.setName(bookDto.getName());
+        book.setCategory(bookDto.getCategory());
+        book.setAvailableCopies(bookDto.getAvailableCopies());
+        return this.bookRepository.save(book);
+
     }
 
     @Override
     public void deleteBook(Long id) {
-
+        this.bookRepository.deleteById(id);
     }
 
     @Override
     public void markBookAsTaken(Long id) {
-
+        Book book=this.bookRepository.findById(id).orElseThrow(()->new RuntimeException());
+        book.setAvailableCopies(book.getAvailableCopies()-1);
+        bookRepository.save(book);
     }
 }
