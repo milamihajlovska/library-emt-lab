eb572a575ef7b1f6d127ed35bfb75d7ef656fc09
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-03-20 20:23:49 +0100

    initial commit

---
+++ src/main/java/mk/finki/ukim/libraryemtlab/LibraryEmtLabApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.libraryemtlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEmtLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEmtLabApplication.class, args);
+	}
+
+}
