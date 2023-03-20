a87b78431dea80281ab790e2bdf92651c184a55a
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-03-20 21:12:11 +0100

    Update AuthorServiceImpl.java

--- src/main/java/mk/finki/ukim/libraryemtlab/service/AuthorServiceImpl.java
+++ src/main/java/mk/finki/ukim/libraryemtlab/service/AuthorServiceImpl.java
@@ -1,35 +1,60 @@
 package mk.finki.ukim.libraryemtlab.service;
 
 import mk.finki.ukim.libraryemtlab.model.Author;
+import mk.finki.ukim.libraryemtlab.model.Country;
 import mk.finki.ukim.libraryemtlab.model.dto.AuthorDto;
+import mk.finki.ukim.libraryemtlab.repository.AuthorRepository;
+import mk.finki.ukim.libraryemtlab.repository.CountryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final CountryRepository countryRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository, CountryRepository countryRepository) {
+        this.authorRepository = authorRepository;
+        this.countryRepository = countryRepository;
+    }
+
     @Override
     public List<Author> findAllAuthors() {
-        return null;
+        return this.authorRepository.findAll();
     }
 
     @Override
     public Author findAuthorById(Long id) {
-        return null;
+        return this.authorRepository.findById(id).orElseThrow(()-> new RuntimeException());
     }
 
     @Override
     public Author addAuthor(AuthorDto authorDto) {
-        return null;
+        Author author=new Author();
+        Country country=countryRepository.findById(authorDto.getCountry()).orElseThrow(()->new RuntimeException());
+        author.setName(authorDto.getName());
+        author.setCountry(country);
+        author.setSurname(authorDto.getSurname());
+
+        return this.authorRepository.save(author);
     }
 
     @Override
     public Author editAuthor(Long id, AuthorDto authorDto) {
-        return null;
+        Author author=authorRepository.findById(id).orElseThrow(()-> new RuntimeException());
+        Country country=countryRepository.findById(authorDto.getCountry()).orElseThrow(()->new RuntimeException());
+        author.setName(authorDto.getName());
+        author.setCountry(country);
+        author.setSurname(authorDto.getSurname());
+
+        return this.authorRepository.save(author);
     }
 
     @Override
     public void deleteAuthor(Long id) {
-
+        authorRepository.deleteById(id);
     }
 }
