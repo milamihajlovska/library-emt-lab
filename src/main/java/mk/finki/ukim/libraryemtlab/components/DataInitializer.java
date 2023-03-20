7ff47d83fb07068edd7418794cffa19d34bbbdce
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-03-20 21:00:12 +0100

    Create DataInitializer.java

---
+++ src/main/java/mk/finki/ukim/libraryemtlab/components/DataInitializer.java
@@ -0,0 +1,56 @@
+package mk.finki.ukim.libraryemtlab.components;
+
+
+import jakarta.annotation.PostConstruct;
+import mk.finki.ukim.libraryemtlab.model.Author;
+import mk.finki.ukim.libraryemtlab.model.Book;
+import mk.finki.ukim.libraryemtlab.model.Category;
+import mk.finki.ukim.libraryemtlab.model.Country;
+import mk.finki.ukim.libraryemtlab.repository.AuthorRepository;
+import mk.finki.ukim.libraryemtlab.repository.BookRepository;
+import mk.finki.ukim.libraryemtlab.repository.CountryRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final CountryRepository countryRepository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public DataInitializer(CountryRepository countryRepository, AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.countryRepository = countryRepository;
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (countryRepository.count() != 0 || authorRepository.count() != 0 || bookRepository.count() != 0) {
+            return;
+        }
+
+        for (int i = 1; i < 11; i++) {
+            create(i);
+        }
+    }
+
+    private void create(int i) {
+        Country c = new Country();
+        c.setName(String.format("Country %d", i));
+        c.setContinent(String.format("Continent %d", i));
+        countryRepository.save(c);
+
+        Author a = new Author();
+        a.setName(String.format("Name %d", i));
+        a.setSurname(String.format("Surname %d", i));
+        a.setCountry(c);
+        authorRepository.save(a);
+
+        Book b = new Book();
+        b.setName(String.format("Name %d", i));
+        b.setCategory(Category.values()[i % Category.values().length]);
+        b.setAuthor(a);
+        b.setAvailableCopies(i);
+        bookRepository.save(b);
+    }
+}
